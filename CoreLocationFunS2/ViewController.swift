5580f00f0d55410e65ba9571e9d19c854c1d2ba4
Gina Sprint <sprint@gonzaga.edu>
2020-11-18 11:43:19 -0800

    Initial Commit

---
+++ CoreLocationFunS2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreLocationFunS2
+//
+//  Created by Gina Sprint on 11/18/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
